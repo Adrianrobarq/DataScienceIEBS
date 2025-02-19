69420ab8a8e60eebb819d25b93e9d99b6b43051b
Adrian Robles Arques <demadrian@gmail.com>
2025-02-19 14:58:01 +0100

    Sprint 2 aprendizaje supervisado 2

--- Bloque 3/Sistemas de almacenamiento/Sprint 2/Sprint_2.sql
+++ Bloque 3/Sistemas de almacenamiento/Sprint 2/Sprint_2.sql
@@ -28,12 +28,12 @@ CREATE TABLE IF NOT EXISTS "Marketing"."Publicitaria" (
     Fecha DATE,
     Visitas BIGINT, 
     Unidades_vendidas BIGINT, 
-    Importe NUMERIC(15, 2), 
-    Inversion_total NUMERIC(15, 2), 
-    Inversion_tv NUMERIC(15, 2), 
-    Inversion_radio NUMERIC(15, 2), 
-    Inversion_social NUMERIC(15, 2))
-TABLESPACE "pg_default";)
+    Importe NUMERIC, 
+    Inversion_tv NUMERIC, 
+    Inversion_radio NUMERIC, 
+    Inversion_social NUMERIC,
+	Inversion_total NUMERIC)
+TABLESPACE "pg_default";
 
 ALTER TABLE IF EXISTS "Marketing"."Publicitaria" 
     OWNER TO postgres;
@@ -44,25 +44,79 @@ COMMENT ON TABLE "Marketing"."Publicitaria"
 -- Generamos tabla con los datos solicitados
 INSERT INTO "Marketing"."Publicitaria" (Fecha, Visitas, Unidades_vendidas, Importe, Inversion_tv, Inversion_radio, inversion_social)
 SELECT
-    DATE("KPI"."Fecha_hora_minuto") AS Fecha,
-    SUM("KPI"."Visitas") AS Visitas,
-    SUM("KPI"."Unidades vendidas") AS Unidades_vendidas,
-    SUM("KPI"."Importe") AS Importe,
-    SUM("TV"."Inversion") AS Inversion_tv,
-    SUM("Radio"."Inversion") AS Inversion_radio,
-    SUM("Social"."Inversion") AS Inversion_social
+	kpi.fecha as fecha, 
+	kpi.visitas as visitas,
+	kpi.Ventas as Unidades_vendidas,
+	kpi.importe as Importe,
+	TV.inversion_tv as Inversion_TV,
+	Radio.inversion_ra as Inversion_radio,
+	Social.inversion_so as inversion_social
 FROM
-    "Marketing"."KPI"
-    FULL JOIN "Marketing"."TV" ON DATE("KPI"."Fecha_hora_minuto") = DATE("TV"."Fecha_hora_minuto")
-    FULL JOIN "Marketing"."Radio" ON DATE("KPI"."Fecha_hora_minuto") = DATE("Radio"."Fecha_hora_minuto")
-    FULL JOIN "Marketing"."Social" ON DATE("KPI"."Fecha_hora_minuto") = DATE("Social"."Fecha")
-GROUP BY
-    Fecha;
+	(SELECT DATE("KPI"."Fecha_hora_minuto") as fecha,
+	SUM("KPI"."Visitas") as visitas,
+	SUM("KPI"."Unidades vendidas") as Ventas,
+	SUM("KPI"."Importe") as Importe
+	FROM "Marketing"."KPI"
+	GROUP BY fecha
+	) kpi
+	LEFT JOIN (SELECT
+	DATE("TV"."Fecha_hora_minuto") as fecha,
+	SUM("TV"."Inversion") as inversion_tv
+	FROM "Marketing"."TV"
+	GROUP BY fecha) TV
+	ON kpi.fecha = TV.fecha
+	LEFT JOIN (SELECT
+	DATE("Radio"."Fecha_hora_minuto") as fecha,
+	SUM("Radio"."Inversion") as inversion_ra
+	FROM "Marketing"."Radio"
+	GROUP BY fecha) Radio
+	ON kpi.fecha = Radio.fecha
+	LEFT JOIN (SELECT
+	"Social"."Fecha" as fecha,
+	SUM("Social"."Inversion") as inversion_so
+	FROM "Marketing"."Social"
+	GROUP BY fecha) Social
+	ON kpi.fecha = social.fecha
+ORDER BY
+	kpi.fecha
+-- Insertamos los null como valor = 0
+UPDATE "Marketing"."Publicitaria"
+SET inversion_tv = 0
+WHERE "Publicitaria".inversion_tv IS NULL;
 
 UPDATE "Marketing"."Publicitaria"
-SET
-    inversion_social = "Social"."Inversion"*"Social".""
+SET inversion_radio = 0 
+WHERE "Publicitaria".inversion_radio IS NULL;
+
+UPDATE "Marketing"."Publicitaria"
+SET inversion_social = 0 
+WHERE "Publicitaria".inversion_social IS NULL;
+
+-- Calculamos la inversión total
+UPDATE "Marketing"."Publicitaria"
+SET inversion_total = "Publicitaria"."inversion_tv" + "Publicitaria"."inversion_radio" + "Publicitaria"."inversion_social";
+
+-- Respondemos las preguntas
+-- ¿En qué medio se ha gastado más presupuesto?
+SELECT
+	SUM(inversion_tv) as total_tv,
+	SUM(inversion_radio) as total_radio,
+	SUM(inversion_social) as total_social
 FROM
-    "Marketing"."Social"
-WHERE 
-    DATE("Publicitaria"."fecha") = DATE("Social"."Fecha");
+	"Marketing"."Publicitaria"
+-- Se gasta más en TV
+-- ¿Qué día se gasta más presupuesto publicitario
+SELECT
+	fecha,
+	inversion_total
+FROM 
+	"Marketing"."Publicitaria"
+ORDER BY
+	inversion_total DESC
+LIMIT 1
+-- El 25/3/2019 con 83581.54€
+
+-- Query de 10 registros
+SELECT *
+FROM "Marketing"."Publicitaria"
+LIMIT 10
